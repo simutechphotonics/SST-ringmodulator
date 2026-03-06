d6d58fcf21f0f40a01e0475deb1e0f0ab4ca1382
Stephen Lin <slin@simutechgroup.com>
2026-03-06 13:49:37 -0800

    test and validated python script

--- device_ringmodulator.py
+++ device_ringmodulator.py
@@ -1,4 +1,8 @@
 import ansys.lumerical.core as lumapi
+import os.path
+
+from pydantic.v1.parse import load_file
+
 
 class DeviceRingModulator:
     def __init__(self):
@@ -11,8 +15,6 @@ class DeviceRingModulator:
         self.ring_gap = 0.2 #um
         self.wavelength_start = 1.5 #um
         self.wavelength_stop = 1.6 #um
-        self.configuration_halfring = False #bool, draws a half_ring device instead
-        self.configuration_racetrack = False #bool, draws a racetrack device instead
         self.ring_racetrack_length = 5 #um, length of the racetrack coupling section
         self.slab_thickness = 0  # um, use 0 for strip waveguides.
         # Simulation region
@@ -23,41 +25,53 @@ class DeviceRingModulator:
         self.material_lowercladding = "SiO2 (Glass) - Palik"
         self.material_wg = "Si (Silicon) - Palik"
 
+    def load_file(self,fspfilename = "device_ringmodulator.fsp"):
+        #creates the fsp file if it doesn't exist, otherwise, loads the existing file.
+        # returns the FDTD object associated with that file
+        if os.path.isfile(fspfilename):
+            return lumapi.FDTD(filename = fspfilename)
+        else:
+            return lumapi.FDTD()
+
     def setup_device(self,fdtd):
-        fdtd.RingFunctions() #this requires RingFunctions.lsf to be in the current directory or search path
+        function = open("RingFunctions.lsf").read() #loads the lsf file
+        #function = "?1+1;?2+2;"
+        fdtd.eval(function) #execute the contents of the lsf file
+        #fdtd.feval("RingFunctions.lsf")
+        # Use setup script's function
         fdtd.setup_RingModulator(
-        thick_Si,
-        width_Si,
-        thick_Clad,
-        thick_BOX,
-        sidewall_angle,
-        ring_radius,
-        gap,
-        wavelength_start,
-        wavelength_stop,
-        racetrack_length,
-        thick_Slab,
-        bool_half_ring,
-        bool_racetrack,
-        sim_width,
-        sim_height,
-        material_Clad,
-        material_BOX,
-        material_Si,
-        Zmin,
-        Zmax
+            self.wg_thickness,
+            self.wg_width,
+            self.uppercladding_thickness,
+            self.lowercladding_thickness,
+            self.sidewall_angle,
+            self.ring_radius,
+            self.ring_gap,
+            self.ring_racetrack_length,
+            self.wavelength_start,
+            self.wavelength_stop,
+            self.slab_thickness,
+            self.sim_width,
+            self.sim_height,
+            self.material_uppercladding,
+            self.material_lowercladding,
+            self.material_wg
         )
 
-    def run_simulation(self):
-        pass
+    def run_simulation(self,fdtd):
+        fdtd.run()
 
-    def run_sparam_sweep(self):
-        pass
+    def run_sparam_sweep(self,fdtd):
+        fdtd.runsweep("SParam")
+        fdtd.exportsweep("SParam","sparam_ringmodulator.dat","lumerical")
 
-    def simulate_fdtd(self):
-        fdtd = lumapi.FDTD  # declare fdtd object
-        self.setup_device(fdtd)
+    def main(self):
+        fdtd = self.load_file()  # get FDTD object
+        self.setup_device(fdtd) # set up device geometry
+        fdtd.save("test.fsp") #save file
+        self.run_sparam_sweep(fdtd)
+        fdtd.close() #close program and release license
 
 if __name__ == "__main__":
     MyRing = DeviceRingModulator()
-    MyRing.simulate_fdtd()
+    MyRing.main()
